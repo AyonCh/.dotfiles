481cbeb153733ef320dab5e066d1a1dfd2eefb47
AyonC <ayon@github.com>
2023-07-05 13:03:20 +0530

    change dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,20 @@
 set -g fish_greeting
+set -U EDITOR nvim
 starship init fish | source
 
+source ~/.config/fish/paths.fish
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+end
+
+function fish_user_key_bindings
+	bind -M insert \ca tmux-sessionizer
 end
 
 function nvm
 	bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
+
 
 function vim
 	nvim $argv
