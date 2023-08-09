b24d55172712bb1c9f36c1e31176bb1abb439708
AyonC <ayon@github.com>
2023-08-09 20:29:18 +0530

    edit fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -19,8 +19,3 @@ nvm use default
 function vim
 	nvim $argv
 end
-
-alias fcd "cd ~ && cd (find * -type d ! -path '*/\.git/*' ! -path '*/node_modules/*' ! -path '*/\.github/*' ! -path '*/\.next/*' ! -path '*/__pycache__/*' | fzf --border rounded --margin 5% --padding 5% --color=fg:#c0caf5,bg:-1,hl:#bb9af7 \
-	--color=fg+:#c0caf5,bg+:#1a1b26,hl+:#7dcfff \
-	--color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff \
-	--color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a,gutter:-1)"
