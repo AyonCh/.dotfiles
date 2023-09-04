2a4bc6a37d8a1b7ba42ba7670364d5b468045a4d
AyonC <ayon@github.com>
2023-09-04 21:32:45 +0530

    edit neovim config

--- nvim/.config/nvim/lua/ayonc/init.lua
+++ nvim/.config/nvim/lua/ayonc/init.lua
@@ -1,3 +1,13 @@
 require("ayonc.options")
 require("ayonc.remap")
 require("ayonc.lazy")
+
+local yankGrp = vim.api.nvim_create_augroup("YankHighlight", {})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 40 })
+	end,
+	group = yankGrp,
+	pattern = "*",
+})
