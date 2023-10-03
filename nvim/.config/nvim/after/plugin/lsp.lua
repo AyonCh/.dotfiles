292ec99f47d3820759e6b9acd7013d6e75f1b61b
AyonC <ayon@github.com>
2023-10-03 14:52:16 +0530

    Update neovim config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -72,21 +72,6 @@ lspconfig["html"].setup({
 lspconfig["tsserver"].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
-	handlers = {
-		["textDocument/publishDiagnostics"] = function(_, _, params, client_id, _, config)
-			if params.diagnostics ~= nil then
-				local idx = 1
-				while idx <= #params.diagnostics do
-					if params.diagnostics[idx].code == 80001 then
-						table.remove(params.diagnostics, idx)
-					else
-						idx = idx + 1
-					end
-				end
-			end
-			vim.lsp.diagnostic.on_publish_diagnostics(_, _, params, client_id, _, config)
-		end,
-	},
 })
 
 lspconfig["cssls"].setup({
