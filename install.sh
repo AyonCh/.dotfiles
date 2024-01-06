eeaef8a55f2f72ac904f3c9b62ab41a9ddc99b4a
AyonC <ayon@github.com>
2024-01-07 01:03:50 +0530

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
-echo "Hello World"
+# #!/bin/bash
+#
+# if [[ -z $(which brew) ]]; then
+#   echo "Installing Brew..."
+#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# fi
+#
+# echo "Installing Git..."
+# brew install git
+# echo "Installing Dotfiles"
+# git clone git@github.com:ayonch/dotfiles.git ~/dotfiles
+# echo "Installing Tools & Dependencies"
+# brew bundle
+# echo "Linking Config"
+# stow */
