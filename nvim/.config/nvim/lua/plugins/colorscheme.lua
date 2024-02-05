4f6e90e8dc97c7c429aada3b0c1610350a3a1584
AyonC <ayon@github.com>
2024-02-05 21:03:00 +0530

    Update neovim config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,46 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-      transparent_background = true,
-      no_italic = false,
-      no_bold = false,
-      highlight_overrides = {
-        mocha = function(C)
-          return {
-            CmpBorder = { fg = C.surface2 },
-            TelescopeBorder = { link = "FloatBorder" },
-            Pmenu = { bg = C.none },
-          }
-        end,
-      },
-    })
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = false,
+        no_italic = false,
+        no_bold = false,
+        highlight_overrides = {
+          mocha = function(C)
+            return {
+              CmpBorder = { fg = C.surface2 },
+              TelescopeBorder = { link = "FloatBorder" },
+              Pmenu = { bg = C.none },
+            }
+          end,
+        },
+      })
 
-    vim.cmd.colorscheme("catppuccin")
-  end,
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "main",
+        styles = {
+          transparency = true,
+        },
+      })
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        transparent = true
+      })
+    end,
+  },
 }
