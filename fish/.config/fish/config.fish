fa22e9e06d2639ce2c7bf6dcb81798200429b58c
AyonC <106944474+AyonCh@users.noreply.github.com>
2025-06-07 01:16:42 +0530

    Update: dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,6 @@
 set -g fish_greeting
 set -U EDITOR nvim
 
-starship init fish | source
-
-source ~/.config/fish/paths.fish
-
 if status is-interactive
 end
 
@@ -12,12 +8,9 @@ function fish_user_key_bindings
   bind -M insert \cf tmux-sessionizer
 end
 
-function nvm
-  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
-end
-
 function vim
   nvim $argv
 end
 
-eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+eval "$(/opt/homebrew/bin/brew shellenv)"
+starship init fish | source
